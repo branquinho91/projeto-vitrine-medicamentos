91c71a91a2e3fa142fdcbb934268b0be376425e7
Gustavo Branquinho <gustavobranquinho2@gmail.com>
2025-01-27 17:22:22 -0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import "reflect-metadata";
+import express from "express";
+import cors from "cors";
+
+import { AppDataSource } from "./database/data-source";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+AppDataSource.initialize()
+  .then(async () => {
+    console.log("Sua conexão com banco de dados está ok");
+    app.listen(3333, () => {
+      console.log("Servidor rodando na porta 3333");
+    });
+  })
+  .catch(() => console.log("Erro ao conectar com o banco de dados"));
